eb511fce29303255511bd3d7744834e35c878224
guidopro <169035608+guidopro@users.noreply.github.com>
2025-10-06 20:05:54 +0100

    update: change firestore rules to create user doc & to allow public viewing of event list

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,15 +1,34 @@
 import { useEffect, useState } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import type { User } from "firebase/auth";
-import { auth } from "../firebase"
+import { auth, db } from "../firebase";
+import { doc, getDoc, setDoc } from "firebase/firestore";
 
 export function useAuth() {
   const [user, setUser] = useState<User | null>(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => {
-      setUser(firebaseUser);
+    const unsubscribe = onAuthStateChanged(auth, async (firebaseUser) => {
+      if (firebaseUser) {
+        // checks if user doc exists
+        const userRef = doc(db, "users", firebaseUser.uid);
+        const userSnap = await getDoc(userRef);
+
+        if (!userSnap.exists()) {
+          // Create a user doc with default role
+          await setDoc(userRef, {
+            name: firebaseUser.displayName,
+            email: firebaseUser.email,
+            role: "member",
+            createdAt: new Date(),
+          });
+          console.log("✅ New user document created");
+        }
+        setUser(firebaseUser);
+      } else {
+        setUser(null);
+      }
       setLoading(false);
     });
 
