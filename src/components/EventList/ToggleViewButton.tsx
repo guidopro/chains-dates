3b68e979cb6d753d32b7c7f0b1099ca44af5ab02
guidopro <169035608+guidopro@users.noreply.github.com>
2025-11-03 10:35:55 +0000

    improve tool-tip text to include whole button

--- src/components/EventList/ToggleViewButton.tsx
+++ src/components/EventList/ToggleViewButton.tsx
@@ -13,18 +13,18 @@ export default function ToggleViewButton({
     <>
       <div className="toggle-button-container">
         {" "}
-        <button onClick={() => setToggleView(!toggleView)}>
+        <button onClick={() => setToggleView(!toggleView)} className="tool-tip">
           {" "}
           {toggleView ? (
-            <div className="tool-tip">
+            <>
               <span className="tool-tip-text">L</span>
               <i className="fa-solid fa-list"></i>
-            </div>
+            </>
           ) : (
-            <div className="tool-tip">
+            <>
               <span className="tool-tip-text">G</span>
               <i className="fa-solid fa-grip"></i>
-            </div>
+            </>
           )}
         </button>
       </div>
